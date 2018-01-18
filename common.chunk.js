fc79421fbb8077b752973222b154986e2eb91443
iMushi <imushi@me.com>
2018-01-18 02:41:40 -0600

    Auto-generated commit

--- common.chunk.js
+++ common.chunk.js
@@ -205,7 +205,7 @@ var PrestoService = (function () {
             xhr.upload.onprogress = function (e) {
                 var percentComplete = Math.ceil((e.loaded / e.total) * 100);
                 console.log('====>', percentComplete);
-                this.next(percentComplete);
+                this.next('Cargando Archivo(s)... ' + percentComplete + '%');
             }.bind(inn);
             xhr.open('POST', url, true);
             xhr.withCredentials = true;
